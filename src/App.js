98c5b8f16734a29c247ec3e7d192fa1b416fd0db
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-01-22 22:22:51 +0300

    Feature: create top bar and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import SideBar from './Components/SideBarComponent/SideBar';
+import TopBar from './Components/TopBarComponent/TopBar'
 
 function App() {
   return (
     <>
-      <SideBar/>
+      <div className='body_container'>
+        <div className='side_bar'>
+        <SideBar/>
+        </div>
+        <div className='top_bar'>
+          <TopBar/>
+        </div>
+      </div>
     </>
   )
 }
